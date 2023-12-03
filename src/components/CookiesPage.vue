74919b1065ae6a596a9a63f96cdfb87042c63c45
Viktoria <chuchkins@yandex.ru>
2023-12-03 16:18:16 +0300

    тест бэка

--- src/components/CookiesPage.vue
+++ src/components/CookiesPage.vue
@@ -1,34 +1,16 @@
 <script lang="ts" setup>
-import type {Ref} from "vue";
-import {computed, ref} from "vue";
+import {ref} from "vue";
 
 interface CookiePrediction {
   name: string;
   description: string;
 }
 
-const cookieList: Ref<CookiePrediction[]> = ref([
-  {
-    name: "Самоуважение",
-    description: "У тебя достаточно сил отпустить те отношения, убеждения или поведение, которые не служат твоему высшему благу."
-  },
-  {name: "Эмоции", description: "Наши эмоции – это неизменный показатель состояния единения со своим истинным «Я»."},
-  { name: "Принцип подобия", description: "Нет случайных людей в нашей жизни. Нас привлекают те люди, у которых те же нерешенные проблемы. Они настроены на нашу частоту и похожи на нас, хотя внешне кажется другими."},
-  { name: "Самопринятие", description: "Мандала воспламеняет оптимизм и уверенность в себе. Честное приятие себя. Там, где свет, обязательно есть тень."},
-  { name: "Соавторство", description: "Мир не нуждается в исправлениях. Творите свою собственную жизнь, позволяя другим людям творить свою."},
-  { name: "Ресурсность", description: "Все уже работает для тебя. Ресурсы уже собраны. Осталось только попасть в Вихрь."},
-  { name: "Внутренний голос", description: "Важно учиться доверять своему внутреннему голосу, чтобы подниматься над страхом отторжения, неправильного понимания или необходимости внешнего подтверждения" +
-        "Аффирмация: Я всегда настроена слышать и понимать свой внутренний голос!"},
-  { name: "Умиротворение", description: "Без истинного мира внутри, мы хотим все подчинить и контролировать, ошибочно думая, что контроль даст нам чувство порядка и безопасности."},
-  { name: "Закон притяжения", description: " Ничего не появляется в нашей жизни просто так. Закон Притяжения реагирует на мысли, которые предлагаете вы."},
-  { name: "Творчество", description: "Освободи место для своей творческой стороны, чтобы впустить веселье, радость и спонтанность."}
-])
-
-function getRandomID() {
-  return Math.floor(Math.random() * cookieList.value.length)
+async function getPrediction() : Promise<CookiePrediction> {
+  return await fetch("/.netlify/functions/getPrediction").then(response => response.json());
 }
 
-const randomCookie = computed(()=> cookieList.value[getRandomID()])
+const randomCookie = await getPrediction();
 
 const tg = window.Telegram?.WebApp;
 
