a40a7dcd12b58b02836eb5516a6bae6c549c5fdc
Viktoria <chuchkins@yandex.ru>
2024-01-08 02:23:44 +0300

    add shake animation

--- src/components/SelectCookie.vue
+++ src/components/SelectCookie.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import {ref} from "vue";
+
 const emit = defineEmits(['selected'])
 
 function getCookie(id: number) {
+  cookieSelected.value[id] = true
+  setTimeout(() => {
+    cookieSelected.value[id] = false
+  }, 1500)
   emit('selected', id)
 }
+
+const cookieSelected  = ref([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+])
 </script>
 
 <template>
@@ -11,8 +29,11 @@ function getCookie(id: number) {
   <div class="flex flex-column background mb-2 mt-2">
     <div class="text-lg mytext text-center">Выбери печеньку на свой вкус</div>
   </div>
-  <div class="flex flex-wrap justify-content-center">
-    <img v-for="n in 9" alt="Cookie" src="../assets/cookie.png" width="100" height="100"
+  <div class="flex flex-wrap justify-content-center cursor-pointer">
+    <img v-for="(v,n) in cookieSelected"
+         alt="Cookie" src="../assets/cookie.png"
+         width="100" height="100"
+         :class="{ shake: v }"
          @click="getCookie(n)"/>
   </div>
 </div>
@@ -25,5 +46,33 @@ function getCookie(id: number) {
 
 .mytext {
   color: var(--tg-theme-text-color);
+}
+
+.shake {
+  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  transform: translate3d(0, 0, 0);
+}
+
+@keyframes shake {
+  10%,
+  90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%,
+  80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%,
+  50%,
+  70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%,
+  60% {
+    transform: translate3d(4px, 0, 0);
+  }
 }
 </style>
